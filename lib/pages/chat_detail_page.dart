cf582fd68cf999e9c421e51ed2ee543936eca2aa
KeingCasabona <keing.limache.casabona@gmail.com>
2024-09-03 19:29:22 -0500

    feat: Add detail chat 1

--- lib/pages/chat_detail_page.dart
+++ lib/pages/chat_detail_page.dart
@@ -5,6 +5,52 @@ class ChatDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leadingWidth: 30,
+        title: Row(
+          children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(
+                'https://images.pexels.com/photos/17767209/pexels-photo-17767209/free-photo-of-sentado-conexion-retrato-comunicacion.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+              ),
+            ),
+            SizedBox(width: 12),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Ximena Lopez Valenzuela',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Last seen today 11:39 AM',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.videocam),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.call),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.more_vert),
+          ),
+        ],
+      ),
+    );
   }
 }
