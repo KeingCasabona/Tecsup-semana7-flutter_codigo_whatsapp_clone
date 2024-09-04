ca3c8a3c8e59c86bbebbd8102f580d9200d4431e
KeingCasabona <keing.limache.casabona@gmail.com>
2024-09-03 20:11:49 -0500

    feat: Add Stack widget

--- lib/pages/chat_detail_page.dart
+++ lib/pages/chat_detail_page.dart
@@ -51,6 +51,40 @@ class ChatDetailPage extends StatelessWidget {
           ),
         ],
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 300,
+              height: 300,
+              color: Colors.blue,
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Positioned(
+                    top: -10,
+                    right: -20,
+                    child: Container(
+                      width: 40,
+                      height: 40,
+                      color: Colors.amber,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      width: 70,
+                      height: 70,
+                      color: Colors.red,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
